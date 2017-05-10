83afb9d4c45417c771f9a755985348d2d0a5bb99
ImoutoChan <kirirrin@gmail.com>
2017-05-10 18:01:19 +0300

    Init

---
+++ IqdbAbuse/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IqdbAbuse
+{
+    static class Extensions
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            Random rnd = new Random();
+            while (n > 1)
+            {
+                int k = (rnd.Next(0, n) % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+
+}
